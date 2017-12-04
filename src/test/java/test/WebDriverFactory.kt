7a041b0f3514dfea2660897043e6b0b75153e159
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-12-05 01:26:37 +0400

    Updated createWebDriver

--- src/test/java/test/WebDriverFactory.kt
+++ src/test/java/test/WebDriverFactory.kt
@@ -7,11 +7,10 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
 import test.WebBrowserCode.*
 
-fun createWebDriver(browserCode: WebBrowserCode): WebDriver =
-    when (browserCode) {
+fun createWebDriver(browser: WebBrowserCode): WebDriver =
+    when (browser) {
         CHROME -> ChromeDriver()
         FIREFOX -> FirefoxDriver()
         EDGE -> EdgeDriver()
         IE -> InternetExplorerDriver()
-        else -> throw IllegalArgumentException("Unexpected browser code.")
     }
