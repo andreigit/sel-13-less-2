d9b7540785790cd9a9a49fb9f131b3b28f805af0
Andrey Sergeev <a.sergeev.official@gmail.com>
2017-12-05 01:42:46 +0400

    Implemented LitecartLoginTestBase in Kotlin

---
+++ src/test/java/test/LitecartLoginTestBase.kt
@@ -0,0 +1,44 @@
+package test
+
+import org.openqa.selenium.By
+import org.openqa.selenium.support.ui.ExpectedConditions.titleIs
+import org.testng.annotations.AfterClass
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.Test
+import java.io.IOException
+
+abstract class LitecartLoginTestBase(browser: WebBrowserCode) : BrowserTestBase(browser) {
+    @BeforeClass
+    fun openBrowser() {
+        try {
+            Runtime.getRuntime().exec(getRunServerString())
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        initDriver()
+    }
+
+    @Test
+    fun checkLitecartLogin() {
+        val ADMIN = "admin"
+
+        driver!!.get("http://localhost/litecart/admin/")
+        driver!!.findElement(By.name("username")).sendKeys(ADMIN)
+        driver!!.findElement(By.name("password")).sendKeys(ADMIN)
+        driver!!.findElement(By.name("login")).click()
+        driverWait!!.until<Boolean>(titleIs("My Store"))
+    }
+
+    @AfterClass
+    fun closeBrowser() {
+        cleanDriver()
+
+        try {
+            Runtime.getRuntime().exec(getStopServerString())
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+}
